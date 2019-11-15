238e73d5995dcdb2ecf97f30b6c556de1e40fd09
the-akira <gabrielfelippe90@gmail.com>
2019-11-15 17:31:58 -0300

    first commit

---
+++ mysite/account/api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path 
+from account.api.views import registration_view, acccount_properties_view, update_account_view
+from rest_framework.authtoken.views import obtain_auth_token
+
+app_name = 'account'
+
+urlpatterns = [
+	path('register', registration_view, name='register'),
+	path('login', obtain_auth_token, name='login'),
+	path('properties', acccount_properties_view, name='properties'),
+	path('properties/update', update_account_view, name='update'),
+]
